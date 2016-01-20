72325dbbc6dad31fa0e613996ffc70ed823d704f
JoelWRussell <jwr111@ic.ac.uk>
2016-01-20 14:07:02 +0000

    Incanter dependencies added

--- project.clj
+++ project.clj
@@ -3,18 +3,19 @@
 ;;;; Released under the MIT license.
 ;;;;
 
-(defproject flow "1.0.0"
-            :url "https://github.com/JonyEpsilon/flow"
+(defproject Goliath "1.0.0"
+            :url "https://github.com/JoelWRussell/Goliath"
             :license {:name "MIT"}
             :dependencies [[org.clojure/clojure "1.7.0-alpha5"]
                            [gorilla-plot "0.1.3"]
                            [algebolic "1.0.0"]
                            [darwin "1.0.0"]
                            [semantic-csv "0.1.0-alpha1"]
-                           [criterium "0.4.3"]]
+                           [criterium "0.4.3"]
+                           [incanter-gorilla "0.1.0"]]
             :plugins [[lein-gorilla "0.3.5"]]
             :jvm-opts ^:replace ["-server"
                                  ;;"-XX:+AggressiveOpts"
                                  ;;"-XX:+UseFastAccessorMethods"
                                  ;;"-XX:+UseCompressedOops"
-                                 "-Xmx4g"])
+                                 "-Xmx1g"])
