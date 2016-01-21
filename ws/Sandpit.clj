f8bfb85717a78d36afbb0848e9957b7fb6adcd76
awm11 <alexmoore_wcs@hotmail.com>
2016-01-21 10:01:01 +0000

    sandpit

---
+++ ws/Sandpit.clj
@@ -0,0 +1,27 @@
+;; gorilla-repl.fileformat = 1
+
+;; **
+;;; # Sandpit
+;;; 
+;;; A place for trying out the cross-over and mutate functions.
+;;; 
+;;; The score function can be called with ts/poly-score
+;;; 
+;; **
+
+;; @@
+(ns gentle-waterfal
+  (:require [gorilla-plot.core :as plot]
+            [regression-test-score :as ts]))
+;; @@
+;; =>
+;;; {"type":"html","content":"<span class='clj-nil'>nil</span>","value":"nil"}
+;; <=
+
+;; @@
+(ts/poly-score )
+;; @@
+
+;; @@
+
+;; @@
