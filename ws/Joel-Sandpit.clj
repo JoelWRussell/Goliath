8c4be9b30b5848bcebdafe63eec2a377d95ade17
JoelWRussell <jwr111@ic.ac.uk>
2016-01-28 12:43:21 +0000

    a few minor changes

--- ws/Joel-Sandpit.clj
+++ ws/Joel-Sandpit.clj
@@ -53,11 +53,19 @@
 (defn splice 
   [parent-gene child]
   (conj child parent-gene))
+g
+;if in both put it into both children 
+;if length of child is the same or greater than the lenght of parent put into other 
+
 
 
 (defn cross-over [poly1 poly2]
-  (let [c1 []
-    	c2 []]  
+  (let [c1 #{}
+    	c2 #{}
+        rnd (rand)] 
+    
+    (if (< (rand) rnd)
+    
 (mapv (fn [poly1 poly2] (conj [(vec x)] (vec y))) poly1 poly2)
    ))
 
@@ -77,18 +85,148 @@
 ;; <=
 
 ;; @@
-(cross-over [1 2 3] [1 2 3])
+(def x [[1 2] [3 5] [7 2] [6 0]] )
+(def y [[3 4] [2 4] [3 5] [1 5]] )
+
+(def c1 [])
+(def c2 [])     
+
+;(mapv (fn [p1 p2 c1 c2]
+ ;       (if (<= rnd (rand))
+  ;        bn        
+   ;       )
+  ;(defn child-allocation
+  ;[parent child-1 child-2]  
+  ;)
+
+        
+(defn add-gene 
+   [parent i child-1 child-2]
+  "Adds the ith gene of parent to child"
+  (println parent)
+  (println child-1)
+  (println child-2)
+  (if (contains? (set child-1) (parent i))
+  (concat child-1 (parent i))
+  (concat child-2 (parent i)))
+   parent child-1 child-2)
+
+(add-gene x 1 c1 c2)
+
+
+
+(if (contains? (set child-1) (x 1) ))
+
+
+  
+
+(defn add-gene 
+   [parent i child-1 child-2]
+  "Adds the ith gene of parent to child"
+  (if (contains? (set child-1) (parent i))
+  (concat child-1 (parent i))
+  (concat child-2 (parent i)))
+   parent child-1 child-2)
 ;; @@
+;; ->
+;;; [[1 2] [3 5] [7 2] [6 0]]
+;;; []
+;;; []
+;;; 
+;; <-
 ;; =>
-;;; {"type":"list-like","open":"<span class='clj-vector'>[</span>","close":"<span class='clj-vector'>]</span>","separator":" ","items":[{"type":"list-like","open":"<span class='clj-vector'>[</span>","close":"<span class='clj-vector'>]</span>","separator":" ","items":[{"type":"list-like","open":"<span class='clj-vector'>[</span>","close":"<span class='clj-vector'>]</span>","separator":" ","items":[{"type":"html","content":"<span class='clj-long'>1</span>","value":"1"},{"type":"html","content":"<span class='clj-long'>2</span>","value":"2"},{"type":"html","content":"<span class='clj-long'>3</span>","value":"3"}],"value":"[1 2 3]"}],"value":"[[1 2 3]]"},{"type":"list-like","open":"<span class='clj-vector'>[</span>","close":"<span class='clj-vector'>]</span>","separator":" ","items":[{"type":"list-like","open":"<span class='clj-vector'>[</span>","close":"<span class='clj-vector'>]</span>","separator":" ","items":[{"type":"html","content":"<span class='clj-long'>1</span>","value":"1"},{"type":"html","content":"<span class='clj-long'>2</span>","value":"2"},{"type":"html","content":"<span class='clj-long'>3</span>","value":"3"}],"value":"[1 2 3]"}],"value":"[[1 2 3]]"}],"value":"[[[1 2 3]] [[1 2 3]]]"}
+;;; {"type":"html","content":"<span class='clj-var'>#&#x27;user/add-gene</span>","value":"#'user/add-gene"}
 ;; <=
 
 ;; @@
-(def x [[1 3] [3 1]])
-(def y [[2 4] [1 0]])
+;(def x [[1 2] [2 4] [4 1]])
+;(def child-x [])
+
+(defn add-gene 
+   [parent i child]
+  "adds allele i from partent to child"
+  (conj child (parent i)))
+
+;(def child-x (add-gene x 1 child-x))
+
+;child-x
 ;; @@
 ;; =>
-;;; {"type":"html","content":"<span class='clj-var'>#&#x27;goliath/y</span>","value":"#'goliath/y"}
+;;; {"type":"list-like","open":"<span class='clj-vector'>[</span>","close":"<span class='clj-vector'>]</span>","separator":" ","items":[{"type":"list-like","open":"<span class='clj-vector'>[</span>","close":"<span class='clj-vector'>]</span>","separator":" ","items":[{"type":"html","content":"<span class='clj-long'>2</span>","value":"2"},{"type":"html","content":"<span class='clj-long'>4</span>","value":"4"}],"value":"[2 4]"}],"value":"[[2 4]]"}
+;; <=
+
+;; @@
+(defn conditional-add 
+  [random-number p1 c1 c2 i]
+  (if (<= random-number (rand))
+       (add-gene p1 i c1)
+       (add-gene p1 i c2)))
+;; @@
+;; =>
+;;; {"type":"html","content":"<span class='clj-var'>#&#x27;user/conditional-add</span>","value":"#'user/conditional-add"}
+;; <=
+
+;; @@
+(def random_num 0.5)
+(def child1 [])
+(def child2 [])
+(def parent [[1 0] [1 1] [3 4]])
+  
+(mapv (fn [x] (if (<= random_num (rand))
+          "child1"
+          "child2")) parent)
+
+(reduce (fn [x y] 
+          (if (<= y (rand))
+          (conj child1 x)
+          (conj child2 x)) random_num parent)
+;; @@
+;; =>
+;;; {"type":"list-like","open":"<span class='clj-vector'>[</span>","close":"<span class='clj-vector'>]</span>","separator":" ","items":[{"type":"html","content":"<span class='clj-string'>&quot;child1&quot;</span>","value":"\"child1\""},{"type":"html","content":"<span class='clj-string'>&quot;child2&quot;</span>","value":"\"child2\""},{"type":"html","content":"<span class='clj-string'>&quot;child2&quot;</span>","value":"\"child2\""}],"value":"[\"child1\" \"child2\" \"child2\"]"}
+;; <=
+
+;; @@
+(if (contains? parent2 x))
+	 () 
+;; @@
+;; =>
+;;; {"type":"html","content":"<span class='clj-long'>25</span>","value":"25"}
+;; <=
+
+;; @@
+(defn distribute-parent-1 
+  [parent-1 child-1 child-2]
+  "Distributes the alleles of parent 1 to children with random probability "
+  (let [rnd-num (rand) 
+        p1 parent-1 
+        c1 child-1
+        c2 child-2
+        length_p (count p1)
+        value 0.0]
+    
+   
+    (if (< length_p (+ value 1))
+      [c1 c2]  
+      (recur (conditional-add rnd-num p1 c1 c2 value) (inc value)))
+    [p1 c1 c2])
+  
+  
+ (func)
+
+
+ (mapv (if )
+;; @@
+
+;; @@
+(def len 4)
+(def value 0)
+
+(if (< len (+ 0 value))
+       value
+    (recur (inc value)))   
+;; @@
+;; =>
+;;; {"type":"html","content":"<span class='clj-var'>#&#x27;user/value</span>","value":"#'user/value"}
 ;; <=
 
 ;; @@
@@ -101,4 +239,25 @@
 
 ;; @@
 
+;; @@
+
+;; @@
+(def parent1 [[1 2] [0 1] [2 0]])
+(def parent2 [[1 4] [1 0] [3 0]])
+
+(def offspring {:child1 []  :child2 []})
+
+;(conj (offspring :child1) [1 0])
+
+
+(defn joel
+  [p1 p2]
+  (loop []))
+;; @@
+;; =>
+;;; {"type":"html","content":"<span class='clj-var'>#&#x27;user/offspring</span>","value":"#'user/offspring"}
+;; <=
+
+;; @@
+(conj offspring)
 ;; @@
